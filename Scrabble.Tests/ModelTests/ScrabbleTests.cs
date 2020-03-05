c4142817c70ee971aaeab0cfa0712387225629db
Student <example@example.com>
2020-03-05 10:40:01 -0800

    add function to find the score for each letter and a test.

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -11,7 +11,46 @@ namespace Letters.Tests
     public void ValidateString_ValidatesString_Bool()
     {
       bool word = ScrabbleScore.ValidateString("string");
-      Assert.IsTrue(word, "word failed");
+      Assert.IsFalse(word, "word failed");
+    }
+
+    [TestMethod]
+    public void toArray_turnsStringToCharArray_CharArray()
+    {
+      string word = "test";
+      char[] testCharArray = new char[] { 't', 'e', 's', 't' };
+      char[] result = ScrabbleScore.toArray(word);
+
+      CollectionAssert.AreEqual(testCharArray, result);
+    }
+
+    [TestMethod]
+    public void ReturnLetterScore_ReturnIntForLetterScore_Int()
+    {
+      char letter = 'a';
+      int score = ScrabbleScore.ReturnLetterScore(letter);
+      Assert.AreEqual(1, score);
+    }
+    [TestMethod]
+    public void ReturnLetterScoreThree_ReturnIntForLetterScoreOfThree_Int()
+    {
+      char letter = 'b';
+      int score = ScrabbleScore.ReturnLetterScore(letter);
+      Assert.AreEqual(3, score);
+    }
+    [TestMethod]
+    public void ReturnLetterScoreTen_ReturnIntForLetterScoreOfTen_Int()
+    {
+      char letter = 'q';
+      int score = ScrabbleScore.ReturnLetterScore(letter);
+      Assert.AreEqual(10, score);
+    }
+    [TestMethod]
+    public void WordScore_ReturnTotalScoreForWord_Int()
+    {
+      string word = "test";
+      int score = ScrabbleScore.WordScore(word);
+      Assert.AreEqual(10, score);
     }
 
   }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -6,8 +6,81 @@ namespace Letters.Models
   {
     public static bool ValidateString(string word)
     {
-      return false;
+      int number;
+      bool isNumber = Int32.TryParse(word, out number);
+
+      return isNumber;
     }
+
+    public static char[] toArray(string word)
+    {
+      char[] result = word.ToCharArray();
+      return result;
+    }
+
+    public static int ReturnLetterScore(char letter)
+    {
+      int score = 0;
+      char letterUpper = char.ToUpper(letter);
+      string scoreOne = "AEIOULNRST";
+      string scoreTwo = "DG";
+      string scoreThree = "BCMP";
+      string scoreFour = "FHVWY";
+      string scoreFive = "K";
+      string scoreEight = "JX";
+      string scoreTen = "QZ";
+
+      // char[] score1 = new char[] { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' };
+      // char[] score2 = new char[] { 'D', 'G' };
+      // char[] score3 = new char[] { 'B', 'C', 'M', 'P' };
+      // char[] score4 = new char[] { 'F', 'H', 'V', 'W', 'Y' };
+      // char[] score5 = new char[] { 'K' };
+      // char[] score8 = new char[] { 'J', 'X' };
+      // char[] score10 = new char[] { 'Q', 'Z' };
+
+
+      if (scoreOne.Contains(letterUpper))
+      {
+        score = 1;
+      }
+      else if (scoreTwo.Contains(letterUpper))
+      {
+        score = 2;
+      }
+      else if (scoreThree.Contains(letterUpper))
+      {
+        score = 3;
+      }
+      else if (scoreFour.Contains(letterUpper))
+      {
+        score = 4;
+      }
+      else if (scoreFive.Contains(letterUpper))
+      {
+        score = 5;
+      }
+      else if (scoreEight.Contains(letterUpper))
+      {
+        score = 8;
+      }
+      else if (scoreTen.Contains(letterUpper))
+      {
+        score = 10;
+      }
+      else
+      {
+        throw new Exception(" Letter not in Alphabet ");
+      }
+
+      return score;
+    }
+
+    public static int WordScore(string word)
+    {
+      int score = 0;
+      return score;
+    }
+
   }
 
 }
